d902fd6dffcf5144ba50f9a3d4935ecfdf0bdda6
John Mathews <john.mathews@air-up.com>
2022-11-16 13:24:17 +0100

    works. needs styling

--- components/Notebook.js
+++ components/Notebook.js
@@ -1,9 +1,7 @@
 import { JupyterNotebookViewer } from "react-jupyter-notebook-viewer"
 
 function Notebook(props) {
-  console.log("--- !*!*!*! debug props: ", props)
   const notebook = new JupyterNotebookViewer(props)
   return <>{notebook}</>
 }
-
 export default Notebook
--- pages/notebooks/[...slug].js
+++ pages/notebooks/[...slug].js
@@ -1,33 +1,33 @@
 import dynamic from "next/dynamic"
 
+import generateRss from "@/lib/generate-rss"
+const root = process.cwd()
+import path from "path"
+
 // https://www.npmjs.com/package/react-jupyter-notebook-viewer
 // https://codesandbox.io/s/nextjs-example-react-jupyter-notebook-viewer-lzjcb5?file=/pages/index.js:0-36
 // https://www.npmjs.com/package/react-jupyter-notebook <- alternative
 
 import fs from "fs"
 
-import {
-  formatNotebookSlug,
-  getNotebookBySlug,
-  getAllNotebooksFrontMatter,
-  getNotebooks,
-} from "@/lib/notebooks"
+import { formatNotebookSlug, getNotebookBySlug, getAllNotebooksFrontMatter } from "@/lib/notebooks"
 
 const Notebook = dynamic(() => import("@/components/Notebook"), {
   ssr: false,
 })
 
 export async function getStaticPaths() {
-  const posts = getNotebooks("notebooks") // list of filenames
+  const posts = await getAllNotebooksFrontMatter("notebooks") // list of filenames
 
   const notDrafts = posts.filter(async (p) => {
-    const post = await getNotebookBySlug("notebooks", formatNotebookSlug(p))
+    const post = await getNotebookBySlug("notebooks", formatNotebookSlug(p.slug))
     return !post.draft
   })
 
   const paths = notDrafts.map((p) => ({
     params: {
-      slug: [formatNotebookSlug(p).split(".")[0]],
+      // slug: [formatNotebookSlug(p.slug).split(".")[0]],
+      slug: [p.slug],
     },
   }))
 
@@ -39,26 +39,53 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   const allPosts = await getAllNotebooksFrontMatter("notebooks")
-  // const thisPost = allPosts.filter((post) => formatNotebookSlug(post.slug) === params.slug.join("/"))[0]
 
   // rss
-  // if (allPosts.length > 0) {
-  //   const rss = generateRss(allPosts)
-  //   fs.writeFileSync("./public/feed.xml", rss)
-  // }
+  if (allPosts.length > 0) {
+    const rss = generateRss(allPosts)
+    fs.writeFileSync("./public/feed.xml", rss)
+  }
 
-  const filePath = `./data/notebooks/${params.slug}.ipynb`
+  const filePath = path.join("/documents", "notebooks", `${params.slug}` + ".ipynb")
 
-  console.log("--- !*!*!*! debug flag ")
-  const source = fs.readFileSync(filePath, "utf8")
-  console.log("--- debug source: ", source)
-
-  return { props: { source, filePath } }
+  console.log("--- debug filePath: ", filePath)
+  return { props: { filePath } }
 }
 
-export default function NotebookPage({ source, filePath, inputLanguage = "python" }) {
+export default function NotebookPage({ filePath, inputLanguage = "python" }) {
   // return source
-  // return <Notebook filePath={"[" + source + "]"} notebookInputLanguage={inputLanguage} />
-  // return <Notebook filePath={source} notebookInputLanguage={inputLanguage} />
-  return <Notebook filePath={filePath} />
+  const DARK_MODE = false
+  return (
+    <Notebook
+      filePath={filePath}
+      // filePath="/notebooks/sqlite.ipynb"
+      notebookInputLanguage={inputLanguage}
+      className="notebook-class"
+      // notebookInputLanguage="python"
+      // notebookOutputLanguage="python"
+      inputCodeDarkTheme={DARK_MODE}
+      outputDarkTheme={DARK_MODE}
+      inputMarkdownDarkTheme={DARK_MODE}
+      showInputLineNumbers={true}
+      showOutputLineNumbers={false}
+      outputTextClassName="output-text"
+      inputTextClassName="input-text"
+      outputBlockClassName="output-block"
+      outputImageClassName="output-image"
+      outputOuterClassName="output-outer"
+      inputOuterClassName="input-outer"
+      outputBorderClassName="output-border"
+      inputBorderClassName="input-border"
+      outputTableClassName="output-table"
+      withOnClick={true}
+      inputMarkdownBlockClassName="input-markdown-block"
+      inputCodeBlockClassName="input-code-block"
+      hideCodeBlocks={false}
+      hideMarkdownBlocks={false}
+      hideAllOutputs={false}
+      hideAllInputs={false}
+      remarkPlugins={[]}
+      rehypePlugins={[]}
+    />
+  )
 }
